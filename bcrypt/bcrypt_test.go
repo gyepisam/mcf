a45706ce1715e2c1ba880f17d34a5415214a08e9
Gyepi Sam <self@gyepi.com>
2014-01-29 23:18:54 +0200

    Added testing

---
+++ bcrypt/bcrypt_test.go
@@ -0,0 +1,126 @@
+package bcrypt
+
+// Test vector copied from https://code.google.com/p/bcryptnet/source/browse/src/BCryptNET.Tests/BCryptTest.cs
+// and edited for Go
+
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"strings"
+
+	"github.com/gyepisam/mcf"
+	"testing"
+)
+
+var testVectors = []struct {
+	plain  string
+	salt   string
+	passwd string
+}{
+	{"",
+		"$2a$06$DCq7YPn5Rq63x1Lad4cll.",
+		"$2a$06$DCq7YPn5Rq63x1Lad4cll.TV4S6ytwfsfvkgY8jIucDrjc8deX1s."},
+	{"",
+		"$2a$08$HqWuK6/Ng6sg9gQzbLrgb.",
+		"$2a$08$HqWuK6/Ng6sg9gQzbLrgb.Tl.ZHfXLhvt/SgVyWhQqgqcZ7ZuUtye"},
+	{"",
+		"$2a$10$k1wbIrmNyFAPwPVPSVa/ze",
+		"$2a$10$k1wbIrmNyFAPwPVPSVa/zecw2BCEnBwVS2GbrmgzxFUOqW9dk4TCW"},
+	{"",
+		"$2a$12$k42ZFHFWqBp3vWli.nIn8u",
+		"$2a$12$k42ZFHFWqBp3vWli.nIn8uYyIkbvYRvodzbfbK18SSsY.CsIQPlxO"},
+	{"a",
+		"$2a$06$m0CrhHm10qJ3lXRY.5zDGO",
+		"$2a$06$m0CrhHm10qJ3lXRY.5zDGO3rS2KdeeWLuGmsfGlMfOxih58VYVfxe"},
+	{"a",
+		"$2a$08$cfcvVd2aQ8CMvoMpP2EBfe",
+		"$2a$08$cfcvVd2aQ8CMvoMpP2EBfeodLEkkFJ9umNEfPD18.hUF62qqlC/V."},
+	{"a",
+		"$2a$10$k87L/MF28Q673VKh8/cPi.",
+		"$2a$10$k87L/MF28Q673VKh8/cPi.SUl7MU/rWuSiIDDFayrKk/1tBsSQu4u"},
+	{"a",
+		"$2a$12$8NJH3LsPrANStV6XtBakCe",
+		"$2a$12$8NJH3LsPrANStV6XtBakCez0cKHXVxmvxIlcz785vxAIZrihHZpeS"},
+	{"abc",
+		"$2a$06$If6bvum7DFjUnE9p2uDeDu",
+		"$2a$06$If6bvum7DFjUnE9p2uDeDu0YHzrHM6tf.iqN8.yx.jNN1ILEf7h0i"},
+	{"abc",
+		"$2a$08$Ro0CUfOqk6cXEKf3dyaM7O",
+		"$2a$08$Ro0CUfOqk6cXEKf3dyaM7OhSCvnwM9s4wIX9JeLapehKK5YdLxKcm"},
+	{"abc",
+		"$2a$10$WvvTPHKwdBJ3uk0Z37EMR.",
+		"$2a$10$WvvTPHKwdBJ3uk0Z37EMR.hLA2W6N9AEBhEgrAOljy2Ae5MtaSIUi"},
+	{"abc",
+		"$2a$12$EXRkfkdmXn2gzds2SSitu.",
+		"$2a$12$EXRkfkdmXn2gzds2SSitu.MW9.gAVqa9eLS1//RYtYCmB1eLHg.9q"},
+	{"abcdefghijklmnopqrstuvwxyz",
+		"$2a$06$.rCVZVOThsIa97pEDOxvGu",
+		"$2a$06$.rCVZVOThsIa97pEDOxvGuRRgzG64bvtJ0938xuqzv18d3ZpQhstC"},
+	{"abcdefghijklmnopqrstuvwxyz",
+		"$2a$08$aTsUwsyowQuzRrDqFflhge",
+		"$2a$08$aTsUwsyowQuzRrDqFflhgekJ8d9/7Z3GV3UcgvzQW3J5zMyrTvlz."},
+	{"abcdefghijklmnopqrstuvwxyz",
+		"$2a$10$fVH8e28OQRj9tqiDXs1e1u",
+		"$2a$10$fVH8e28OQRj9tqiDXs1e1uxpsjN0c7II7YPKXua2NAKYvM6iQk7dq"},
+	{"abcdefghijklmnopqrstuvwxyz",
+		"$2a$12$D4G5f18o7aMMfwasBL7Gpu",
+		"$2a$12$D4G5f18o7aMMfwasBL7GpuQWuP3pkrZrOAnqP.bmezbMng.QwJ/pG"},
+	{"~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
+		"$2a$06$fPIsBO8qRqkjj273rfaOI.",
+		"$2a$06$fPIsBO8qRqkjj273rfaOI.HtSV9jLDpTbZn782DC6/t7qT67P6FfO"},
+	{"~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
+		"$2a$08$Eq2r4G/76Wv39MzSX262hu",
+		"$2a$08$Eq2r4G/76Wv39MzSX262huzPz612MZiYHVUJe/OcOql2jo4.9UxTW"},
+	{"~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
+		"$2a$10$LgfYWkbzEvQ4JakH7rOvHe",
+		"$2a$10$LgfYWkbzEvQ4JakH7rOvHe0y8pHKF9OaFgwUZ2q7W2FFZmZzJYlfS"},
+	{"~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
+		"$2a$12$WApznUOJfkEGSmYRfnkrPO",
+		"$2a$12$WApznUOJfkEGSmYRfnkrPOr466oFDCaj4b6HY3EXGvfxm43seyhgC"},
+}
+
+func setSalt(salt []byte) {
+}
+
+func TestVectors(t *testing.T) {
+	for i, v := range testVectors {
+
+		x := strings.Split(v.salt[1:], "$")
+		if len(x) != 3 {
+			t.Fatalf("%d: invalid field structure: %s", i, v.salt)
+		}
+		costIn, saltIn := x[1], x[2] //index 0 is not interesting.
+
+		salt, err := base64Decode([]byte(saltIn))
+
+		if err != nil {
+			t.Fatalf("%d: error decoding salt: %s", i, err)
+		}
+		// Horrible hack to ensure that crypto/bcrypt uses my salt.
+		rand.Reader = bytes.NewReader(salt)
+
+		var cost int
+		if _, err := fmt.Sscanf(costIn, "%02d", &cost); err != nil {
+			t.Fatalf("%d: error decoding param [%+v]: %s", i, costIn, err)
+		}
+		SetCost(cost)
+
+		t.Logf("%d: plain: %q, salt: %q, cost: %d\n", i, v.plain, saltIn, cost)
+
+		got, err := mcf.Generate(v.plain)
+		if err != nil {
+			// password must be at least 2 bytes otherwise crypto/blowfish complains.
+			// This should be handled in bcrypt
+			if len(v.plain) < 3 {
+				continue
+			}
+			t.Errorf("%d: unexpected error: %s", i, err)
+			continue
+		}
+
+		if want := v.passwd; want != got {
+			t.Errorf("%d: output mismatch. want: %s, got %s", i, want, got)
+		}
+	}
+}
