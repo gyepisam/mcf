7f34d4d74c172ecec19db300105fd6b09e7fd5bc
Gyepi Sam <self@gyepi.com>
2014-01-30 21:25:08 +0200

    Update documentation, add copyright header

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -1,5 +1,9 @@
+// Copyright 2014 Gyepi Sam. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
 // Package encoder represents an interface that MCF password encoders must implement
-// and provides a bridge Encoder implementation to simplify concrete implementations.
 package encoder
 
 // An Encoder encodes a plaintext password into a hashed format.
